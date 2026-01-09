97f97df28a99607c5b6c815f00e592913c4ae138
borsodiadam <borsodi.dm@gmail.com>
2026-01-09 11:17:06 +0100

    images

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       { hostname: 'images.react-photo-album.com' },
       { protocol: 'https', hostname: 'res.cloudinary.com' },
